3a714742d91a8862144074d07b89b44d4186aac2
Om Chauhan <chauhanom21@gmail.com>
2023-05-07 20:04:15 +0530

    chore: kafka ssl client

--- main.go
+++ main.go
@@ -1,189 +1,65 @@
 package main
 
-// import (
-// 	"fmt"
-
-// 	"os"
-
-// 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
-// )
-
-// // GetEnvParam : return string environmental param if exists, otherwise return default
-// func GetEnvParam(param string, dflt string) string {
-// 	if v, exists := os.LookupEnv(param); exists {
-// 		return v
-// 	}
-// 	return dflt
-// }
-
-// func main() {
-// 	fmt.Println("system service is ready to read from Kafka...")
-// 	host := GetEnvParam("KAFKA_HOST", "localhost")
-// 	port := GetEnvParam("KAFKA_PORT", "9092")
-// 	topic := GetEnvParam("KAFKA_TOPIC", "demoTopic")
-// 	maxPollInterval := GetEnvParam("KAFKA_MAX_POLL_INTERVAL_MS", "60000")
-
-// 	fmt.Printf("Kafka host:%s , ,port:%s, ,topic:%s \n", host, port, topic)
-// 	kafkaServer := host + ":" + port
-
-// 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
-// 		"bootstrap.servers":    kafkaServer,
-// 		"group.id":             "myGroup",
-// 		"auto.offset.reset":    "earliest",
-// 		"max.poll.interval.ms": maxPollInterval,
-// 	})
-
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	c.SubscribeTopics([]string{topic}, nil)
-
-// 	// A signal handler or similar could be used to set this to false to break the loop.
-// 	run := true
-
-// 	for run {
-// 		ev := c.Poll(1000)
-// 		switch e := ev.(type) {
-// 		case *kafka.Message:
-// 			// application-specific processing
-// 			if e.TopicPartition.Error != nil {
-// 				fmt.Printf("Failed to deliver message: %v\n", e.TopicPartition.Error)
-// 			} else {
-// 				fmt.Printf("Received record from topic %s partition [%d] : %v\n", *e.TopicPartition.Topic, e.TopicPartition.Partition, string(e.Value))
-// 			}
-// 		case kafka.Error:
-// 			fmt.Printf("Consumer error: %v (%v)\n", err, ev)
-// 			run = false
-// 		default:
-// 			//fmt.Printf("Ignored %v\n", e)
-// 		}
-// 	}
-
-// 	c.Close()
-// }
-
 import (
+	"context"
 	"crypto/tls"
-	"crypto/x509"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
-	"os/signal"
 	"strings"
-	"sync"
+	"time"
 
-	"github.com/Shopify/sarama"
+	kafka "github.com/segmentio/kafka-go"
 )
 
-func main() {
-
+func getKafkaReader(kafkaURL, topic, groupID string) *kafka.Reader {
 	clientCertFile := GetEnvParam("KAFKA_CLIENT_CERT", "/home/om/go/src/github.com/subhamproject/devops-demo/certs/kafka.system.cert")
 	clientKeyFile := GetEnvParam("KAFKA_CLIENT_KEY", "/home/om/go/src/github.com/subhamproject/devops-demo/certs/kafka.system.key")
-	caCertFile := GetEnvParam("KAFKA_CA_CERT", "/home/om/go/src/github.com/subhamproject/devops-demo/certs/kafka.system.pem")
-	kafkaservers := GetEnvParam("KAFKA_SERVERS", "kafka1:9091")
-	servers := strings.Split(kafkaservers, ",")
+	// caCertFile := GetEnvParam("KAFKA_CA_CERT", "/home/om/go/src/github.com/subhamproject/devops-demo/certs/kafka.user.pem")
 
-	fmt.Println("kafka servers: ", servers)
-	tlsConfig, err := NewTLSConfig(clientCertFile, clientKeyFile, caCertFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// This can be used on test server if domain does not match cert:
-	// tlsConfig.InsecureSkipVerify = true
-
-	consumerConfig := sarama.NewConfig()
-	consumerConfig.Net.TLS.Enable = true
-	consumerConfig.Net.TLS.Config = tlsConfig
-
-	client, err := sarama.NewClient(servers, consumerConfig)
-	if err != nil {
-		log.Fatalf("unable to create kafka client: %q", err)
-	}
-
-	consumer, err := sarama.NewConsumerFromClient(client)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer consumer.Close()
-
-	consumerLoop(consumer, "demoTopic")
-}
-
-// NewTLSConfig generates a TLS configuration used to authenticate on server with
-// certificates.
-// Parameters are the three pem files path we need to authenticate: client cert, client key and CA cert.
-func NewTLSConfig(clientCertFile, clientKeyFile, caCertFile string) (*tls.Config, error) {
-	tlsConfig := tls.Config{}
-
-	// Load client cert
 	cert, err := tls.LoadX509KeyPair(clientCertFile, clientKeyFile)
 	if err != nil {
-		return &tlsConfig, err
+		log.Fatal(err)
 	}
-	tlsConfig.Certificates = []tls.Certificate{cert}
-
-	// Load CA cert
-	caCert, err := ioutil.ReadFile(caCertFile)
 	if err != nil {
-		return &tlsConfig, err
+		log.Fatal(err)
 	}
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(caCert)
-	tlsConfig.RootCAs = caCertPool
+	cfg := &tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
 
-	tlsConfig.BuildNameToCertificate()
-	return &tlsConfig, err
+	dialer := &kafka.Dialer{
+		Timeout:   10 * time.Second,
+		DualStack: true,
+		TLS:       cfg,
+	}
+	brokers := strings.Split(kafkaURL, ",")
+	return kafka.NewReader(kafka.ReaderConfig{
+		Brokers:  brokers,
+		GroupID:  groupID,
+		Topic:    topic,
+		MaxBytes: 10e6, // 10MB
+		Dialer:   dialer,
+	})
 }
 
-func consumerLoop(consumer sarama.Consumer, topic string) {
-	partitions, err := consumer.Partitions(topic)
-	if err != nil {
-		log.Println("unable to fetch partition IDs for the topic", topic, err)
-		return
-	}
+func main() {
+	// get kafka reader using environment variables.
+	groupID := GetEnvParam("KAFKA_GROUP_ID", "demo-kafka")
+	topic := GetEnvParam("KAFKA_TOPIC", "demoTopic")
+	kafkaURL := GetEnvParam("KAFKA_SERVERS", "localhost:19091,localhost:29092,localhost:39093")
+	fmt.Println("kafka kafkaURL: ", kafkaURL)
 
-	// Trap SIGINT to trigger a shutdown.
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt)
+	kafkaReader := getKafkaReader(kafkaURL, groupID, topic)
 
-	var wg sync.WaitGroup
-	for partition := range partitions {
-		wg.Add(1)
-		go func() {
-			consumePartition(consumer, int32(partition), signals)
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-}
+	defer kafkaReader.Close()
 
-func consumePartition(consumer sarama.Consumer, partition int32, signals chan os.Signal) {
-	log.Println("Receving on partition", partition)
-	partitionConsumer, err := consumer.ConsumePartition("test", partition, sarama.OffsetNewest)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer func() {
-		if err := partitionConsumer.Close(); err != nil {
-			log.Println(err)
-		}
-	}()
-
-	consumed := 0
-ConsumerLoop:
+	fmt.Println("start consuming ... !!")
 	for {
-		select {
-		case msg := <-partitionConsumer.Messages():
-			log.Printf("Consumed message offset %d\nData: %s\n", msg.Offset, msg.Value)
-			consumed++
-		case <-signals:
-			break ConsumerLoop
+		m, err := kafkaReader.ReadMessage(context.Background())
+		if err != nil {
+			log.Fatalln(err)
 		}
+		fmt.Printf("message at topic:%v partition:%v offset:%v	%s = %s\n", m.Topic, m.Partition, m.Offset, string(m.Key), string(m.Value))
 	}
-	log.Printf("Consumed: %d\n", consumed)
 }
 
 // GetEnvParam : return string environmental param if exists, otherwise return default
